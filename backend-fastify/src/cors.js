2e4f5d321cb3eba9dde987e4d8a590aa90689fba
Sonaladm <goursonal179@gmail.com>
2024-06-07 00:37:05 +0530

    Update cors.js

--- backend-fastify/src/cors.js
+++ backend-fastify/src/cors.js
@@ -6,7 +6,7 @@ export const setFastifyCors = function (fastify) {
     origin: [
       "http://localhost:9000",
       "http://localhost:8100",
-      "http://localhost:4200",
+      "http://13.208.79.151:4200",
     ],
   });
 };
