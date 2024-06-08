72ddfef06c7457a8c77dbbc055883c8142572f50
Sonaladm <goursonal179@gmail.com>
2024-06-08 19:16:32 +0530

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api: {
-    server: 'http://localhost:8000/',
+    server: 'http://15.152.236.53:8000/',
     mapKey: '',
     googleAuthClientId: '726428814886-pq28ioib6dskevnhk3mf74kb5ftqbo94.apps.googleusercontent.com',
     webSocketUrl: ''
